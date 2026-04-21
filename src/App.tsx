9ef3912367fb8e34ede5afd23b0247a303dfd42b
Balay Kumar Bagchi <bkbagchi.dipto@gmail.com>
2026-04-21 17:57:50 +0600

    feat: shadcn ui toast notification enabled

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "@/router";
+import { Toaster } from "@components/ui/sonner";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Toaster />
       <AppRouter />
     </BrowserRouter>
   );
