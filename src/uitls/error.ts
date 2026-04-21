ffa76488771fce9b3047d1a69abaa65654e6001c
Balay Kumar Bagchi <bkbagchi.dipto@gmail.com>
2026-04-21 17:55:36 +0600

    feat: utils added to type cast response error message

---
+++ src/uitls/error.ts
@@ -0,0 +1,16 @@
+export type ApiError = {
+  response: {
+    data: {
+      message: string;
+    };
+  };
+};
+
+export const getErrorMessage = (error: unknown): string => {
+  if (typeof error === "object" && error !== null && "response" in error) {
+    const err = error as ApiError;
+    return err.response?.data?.message ?? "Something went wrong";
+  }
+
+  return "Something went wrong";
+};
