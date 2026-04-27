23b465bd5113f46b9c9660d9fdf985bb0809fff6
Balay Kumar Bagchi <bkbagchi.dipto@gmail.com>
2026-04-27 12:50:28 +0600

    update: template types updated

--- src/types/template.ts
+++ src/types/template.ts
@@ -1,7 +1,12 @@
+import type { Project } from "./project";
+import type { Service } from "./service";
+
 export interface Template {
   id: string;
   userId: string;
+  project: Project;
   projectId: string;
+  service: Service;
   serviceId: string;
   name: string;
   description: string;
